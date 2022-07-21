9e0204d3c9971cf9f9f8bbcd47bf7a973c1af598
Rex James R. Pinili <rexjamesp@gmail.com>
2022-07-21 16:17:48 +0800

    feat: Page Change in Questions List

--- app/src/main/java/com/stackexchangelite/app/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/stackexchangelite/app/ui/activities/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,7 @@ import butterknife.ButterKnife
 import com.bumptech.glide.RequestManager
 import com.stackexchangelite.app.R
 import com.stackexchangelite.app.data.model.Item
+import com.stackexchangelite.app.data.model.Welcome10
 import com.stackexchangelite.app.ui.activities.Resource
 import com.stackexchangelite.app.ui.adapter.QuestionsAdapter
 import com.stackexchangelite.app.ui.adapter.QuestionsAdapterEvents
@@ -19,7 +21,7 @@ import com.stackexchangelite.app.ui.viewmodels.ViewModelProviderFactory
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
-class MainActivity : DaggerAppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity(), View.OnClickListener {
 
     private val TAG = "MainActivity"
 
@@ -32,6 +34,12 @@ class MainActivity : DaggerAppCompatActivity() {
     @BindView(R.id.layout_error)
     lateinit var errorLayout: FrameLayout
 
+    @BindView(R.id.btn_prev)
+    lateinit var btnPrev: TextView
+
+    @BindView(R.id.btn_next)
+    lateinit var btnNext: TextView
+
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
 
@@ -40,6 +48,8 @@ class MainActivity : DaggerAppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
     lateinit var questionsAdapter: QuestionsAdapter
+
+    private var pageNumber: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,32 +67,55 @@ class MainActivity : DaggerAppCompatActivity() {
 
         viewModel = ViewModelProvider(this, providerFactory)[MainViewModel::class.java]
         viewModel.getQuestionsData().observe(this) {
-            errorLayout.visibility = View.GONE
-            progressLayout.visibility = View.GONE
-            when (it.status) {
-                Resource.Status.LOADING -> {
-                    Log.d(TAG, "init: Fetching data")
-                    progressLayout.visibility = View.VISIBLE
-                }
-                Resource.Status.SUCCESS -> {
-                    Log.d(TAG, "init: ${it.data.items}")
-                    progressLayout.visibility = View.GONE
-                    if (it.data.items != null) {
-                        questionsAdapter.updateQuestionList(it.data.items)
-                        questionsAdapter.notifyDataSetChanged()
-                    }
-                }
-                Resource.Status.ERROR -> {
-                    progressLayout.visibility = View.GONE
-                    errorLayout.visibility = View.VISIBLE
-                }
-            }
+            resetUIState()
+            setUIState(it)
         }
-        viewModel.pullQuestions(1)
-
-
+        viewModel.pullQuestions(pageNumber)
 
         recyclerQuestionList.layoutManager = LinearLayoutManager(this)
         recyclerQuestionList.adapter = questionsAdapter
+
+        btnPrev.setOnClickListener(this)
+        btnNext.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.btn_prev -> {
+                if (pageNumber > 1) {
+                    viewModel.pullQuestions(--pageNumber)
+                }
+            }
+            R.id.btn_next -> { viewModel.pullQuestions(++pageNumber) }
+        }
+    }
+
+    private fun resetUIState() {
+        errorLayout.visibility = View.GONE
+        progressLayout.visibility = View.GONE
+        btnPrev.visibility = if (pageNumber <= 1) View.INVISIBLE else View.VISIBLE
+        (recyclerQuestionList.layoutManager as LinearLayoutManager).scrollToPosition(0)
+    }
+
+    private fun setUIState(resource: Resource<Welcome10>) {
+        when (resource.status) {
+            Resource.Status.LOADING -> {
+                Log.d(TAG, "setUIState: Fetching data...")
+                progressLayout.visibility = View.VISIBLE
+            }
+            Resource.Status.SUCCESS -> {
+                Log.d(TAG, "setUIState: ${resource.data.items}")
+                progressLayout.visibility = View.GONE
+                if (resource.data.items != null) {
+                    questionsAdapter.updateQuestionList(resource.data.items)
+                    questionsAdapter.notifyDataSetChanged()
+                }
+            }
+            Resource.Status.ERROR -> {
+                Log.d(TAG, "setUIState: Failed to fetch data!")
+                progressLayout.visibility = View.GONE
+                errorLayout.visibility = View.VISIBLE
+            }
+        }
     }
 }
